dbd2213cb9fca150699892f653b8291808cdaaa8
Shreyas Pahune <pahuneshreyas@gmail.com>
2022-06-17 15:52:08 +0530

    minor changes in backend

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const userSchema = require("../model/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const authorizeUser = require('../middlewares/auth');
+const authorizeUser = require("../middlewares/auth");
 const { response } = require("express");
 
 // Sign In / Login
@@ -13,11 +13,11 @@ router.post("/login", async (req, res, next) => {
   try {
     const secretKey = process.env.JWT_SECRET;
     const { email, password } = req.body;
-    if(!email || !password){
-      return res.status(422).json({message: "Please fill all the data! 🔴"})
+    if (!email || !password) {
+      return res.status(422).json({ message: "Please fill all the data! 🔴" });
     }
     const findUser = await userSchema.findOne({
-      email: email
+      email: email,
     });
     if (!findUser) {
       return res.status(401).json({ message: "Email not found! 🔴" });
@@ -32,7 +32,7 @@ router.post("/login", async (req, res, next) => {
     );
     return res
       .status(200)
-      .json({ message: "User logged in! 🟢", token: token ,details: findUser });
+      .json({ message: "User logged in! 🟢", token: token, details: findUser });
   } catch (error) {
     return res
       .status(401)
@@ -42,45 +42,42 @@ router.post("/login", async (req, res, next) => {
 
 // sign up / registration of a new user
 
-router.post('/register', async (req, res, next) => {
-    try {
-        const { email, password, name } = req.body;
-          if(!email || !password || !name){
-            
-            return res.status(422).json({message: "Please fill all the data! 🔴"})
-          }
-          const findUser = await userSchema.findOne({email});
-          if(findUser){
-            res.status(409).json({message: "user is already present."})
-          }
-          const hashedPass = await bcrypt.hash(password, 12);
-          const newUser =await userSchema.create({
-            name, email, password: hashedPass
-          })
-          
-          if(!newUser){
-            res.json({message: "Some error occurred during registration of the user! 🔴"})
-          }
-          res.status(201).json({message: "User created! 🟢", details: newUser})
-          
-        
-    } catch (error) {
-        return res
+router.post("/register", async (req, res, next) => {
+  try {
+    const { email, password, name } = req.body;
+    if (!email || !password || !name) {
+      return res.status(422).json({ message: "Please fill all the data! 🔴" });
+    }
+    const findUser = await userSchema.findOne({ email });
+    if (findUser) {
+      res.status(409).json({ message: "user is already present." });
+    }
+    const hashedPass = await bcrypt.hash(password, 12);
+    const newUser = await userSchema.create({
+      name,
+      email,
+      password: hashedPass,
+    });
+
+    if (!newUser) {
+      res.json({
+        message: "Some error occurred during registration of the user! 🔴",
+      });
+    }
+    res.status(201).json({ message: "User created! 🟢", details: newUser });
+  } catch (error) {
+    return res
       .status(401)
       .json({ message: "Some error occurred in register route! 🔴" });
-    }
- 
-})
+  }
+});
 
-router.route('/all-users').get(authorizeUser, (req, res, next) => {
+router.route("/all-users").get(authorizeUser, (req, res, next) => {
   userSchema.find((err, data) => {
-    if(err){
-      return next(err)
+    if (err) {
+      return next(err);
     }
-    res.status(200).json(data)
+    res.status(200).json(data);
   });
-})
-module.exports = router
-
-
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9uZUBnbWFpbC5jb20iLCJ1c2VySWQiOiI2MmFiYWI4MDA3ZjI5MzhiNDYyZGNiOGUiLCJpYXQiOjE2NTU0MTg0Mjl9.VizXl2kLG6R8I6_7epFq4pMFVTFZGFNMSJjYO31jtdY
+});
+module.exports = router;
