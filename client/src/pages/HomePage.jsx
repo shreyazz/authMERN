7f28a913864a3f19e1eb7c503532106f52b16764
Shreyas Pahune <pahuneshreyas@gmail.com>
2022-06-19 14:08:34 +0530

    added jwt decode to homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button'
 import { Link } from "react-router-dom";
+import jwt_decode from 'jwt-decode';
 const HomePage = () => {
   const logout = () => {
     localStorage.removeItem('token');
+
     window.location.reload('')
   }
+  const [userDetails, setUserDetails] = useState({})
+  useEffect(() => {
+  setUserDetails(jwt_decode(localStorage.getItem('token')))
+  }, [])
   return (
     <>
      <Navbar bg="dark" variant="dark">
@@ -17,10 +23,10 @@ const HomePage = () => {
           <Nav className="me-auto">
             <Link to={'/secret'}>Admin Page</Link>
             <Button variant="info" onClick={logout}>Log out</Button>
-
           </Nav>
         </Container>
       </Navbar>
+      <h1>Hey there, <span>{userDetails.name}</span></h1>
     </>
   );
 };
